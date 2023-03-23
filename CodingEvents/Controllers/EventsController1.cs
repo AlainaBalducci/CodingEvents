5b733a55c1e7f2441b98f32dbddc3bd9c2ba6157
Alaina Balducci <alainabalducci@gmail.com>
2023-03-23 10:03:40 -0500

    finished

---
+++ CodingEvents/Controllers/EventsController1.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CodingEvents.Controllers
+{
+    
+    public class EventsController1 : Controller
+    {
+        private static List<string> Events = new List<string>();
+        [Route("/Events")]
+        public IActionResult Index()
+        {
+           
+            ViewBag.events = Events;
+
+            return View();
+        }
+        [HttpGet]
+        [Route("/Events/Add")]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("Events/Add")]
+        public IActionResult NewEvent(string name)
+        {
+            Events.Add(name);
+            return Redirect("/Events");
+        }
+
+        
+    }
+}
