13533f0749a6da4202d3374ddf2e2cd9eec72d5c
Alaina Balducci <alainabalducci@gmail.com>
2023-04-06 11:36:52 -0500

    Started studio

--- CodingEvents/Controllers/EventCategoryController.cs
+++ CodingEvents/Controllers/EventCategoryController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using CodingEvents.Data;
 using CodingEvents.Models;
+using CodingEvents.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -25,6 +26,34 @@ namespace CodingEvents.Controllers
             List<EventCategory> categories = context.Categories.ToList();
             return View(categories);
 
+        }
+
+        [HttpGet]
+        [Route("EventCategory/Create")]
+        public IActionResult Create() 
+        {
+            AddEventCategoryViewModel addEventCategoryViewModel = new AddEventCategoryViewModel();
+            return View();
+        }
+
+        [HttpPost]
+        [Route("EventCategory/Create")]
+        public IActionResult ProcessCreateEventCategoryForm(AddEventCategoryViewModel addEventCategoryViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                EventCategory newCategory = new EventCategory
+                {
+                    Name = addEventCategoryViewModel.Name,
+                  
+                };
+
+                context.Categories.Add(newCategory);
+                context.SaveChanges();
+                return Redirect("EventCategory");
+            }
+
+            return View("EventCategory/Create");
         }
     }
 }
---
+++ CodingEvents/ViewModels/AddEventCategoryViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CodingEvents.ViewModels
+{
+    public class AddEventCategoryViewModel
+    {
+
+        [Required(ErrorMessage ="Please enter an event category")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage ="Please enter an event category between 3 and 20 characters long")]
+        public string? Name { get; set; }
+    }
+}
