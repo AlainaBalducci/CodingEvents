48dbe1247e18f9f8567f18b78463d9cef9bfd72c
Alaina Balducci <alainabalducci@gmail.com>
2023-04-07 10:58:33 -0500

    added tag class

---
+++ CodingEvents/Models/Tag.cs
@@ -0,0 +1,20 @@
+﻿namespace CodingEvents.Models
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public List<Event> Events { get; set; }
+
+
+        public Tag()
+        {
+        }
+
+        public Tag(string name)
+        {
+            Name = name;
+        }
+    }
+}
